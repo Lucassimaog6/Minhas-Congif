49f702b5c4f01ffb7df03974158ed80c434b82f1
Lucassimaog6 <22101918@aluno.cotemig.com.br>
2022-05-26 08:49:53 -0300

    first commit

---
+++ config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias n="nvim"
+
+
+alias pi="sudo pacman -S"
+alias pu="sudo pacman -Su"
+alias pr="sudo pacman -R"
+alias ps="sudo pacman -Ss"
+
+alias yi="yay -S"
+alias yu="yay -Su"
+alias yr="yay -R"
+alias ys="yay -Ss"
